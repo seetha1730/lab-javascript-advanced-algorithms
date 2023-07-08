7dc2224dbeb0dcf3a1bb8b230ee20c04e86d0635
“seetha1730” <seetharaj1990@gmail.com>
2023-07-08 12:24:10 +0200

    solved stack and queue

--- src/queue/queue-dom.js
+++ src/queue/queue-dom.js
@@ -10,37 +10,64 @@ const dequeue = document.querySelector('.btn-take-dequeue');
 const queue = new Queue();
 
 const clearQueueInput = () => {
+  queueInput.value = '';
   // ... your code goes here
 };
 
 const generateListQueue = () => {
   // ... your code goes here
+  warningTopQueue.style.display = 'none';
+  warningBottomQueue.style.display = 'none';
+  queueUL.innerHTML = '';
+  let length = queue.display().length;
+  let size = queue.MAX_SIZE - length;
+  queue.display().forEach(item => {
+    let li = document.createElement('li');
+    li.className = 'active';
+    li.innerText = item || '';
+    queueUL.appendChild(li);
+  });
+  for (let i = 0; i < size; i++) {
+    let li = document.createElement('li');
+    li.className = 'inactive';
+    li.innerHTML = '&nbsp;';
+    queueUL.appendChild(li);
+  }
 };
 
 generateListQueue();
 
 const generateWarningQueue = (type) => {
   if (type === 'underflow') {
+    //generateWarningStack('overflow');
     // ... your code goes here
+    warningBottomQueue.style.display = 'block';
+    warningBottomQueue.innerText = type;
   } else if (type === 'overflow') {
     // ... your code goes here
+    warningTopQueue.style.display = 'block';
+    warningTopQueue.innerText = type;
   }
 };
 
 const addToQueue = () => {
-  try {
-    // ... your code goes here
-  } catch (error) {
-    // there was an overflow error, handle it
+ if (queue.enqueue() === 'Queue Overflow') {
+  generateWarningQueue('overflow');
+  } else {
+    clearQueueInput();
+    generateListQueue();
   }
+   
 };
 
 const removeFromQueue = () => {
-  try {
-    // ... your code goes here
-  } catch (error) {
-    // there was an underflow error, handle it
+  if (queue.dequeue() === 'Queue Underflow') {
+    generateWarningQueue('Underflow');  
+  } else {
+    clearQueueInput();
+    generateListQueue();
   }
+   
 };
 
 addQueue.addEventListener('click', addToQueue);
--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,25 +5,49 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    //if( this.stackControl.length === this.MAX_SIZE
+    if (
+      this.stackControl.length === 0 ||
+      this.stackControl.length < this.MAX_SIZE
+    ) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (this.stackControl.length == 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()) {
+     // console.log(this.stackControl.push(item));
+       this.stackControl.push(item);
+       return this.stackControl;
+    } else {
+      throw new Error("STACK_OVERFLOW");
+    }
   }
 
   pop() {
-    // ... your code goes here
+    if(this.stackControl.length ===0 ){
+      throw new Error("STACK_UNDERFLOW");
+    }else{
+      return this.stackControl.pop();
+    }
+    
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl;
+    
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Stack;
+if (typeof module !== "undefined") module.exports = Stack;
